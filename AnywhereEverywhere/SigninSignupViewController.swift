ad74612c0aed330051a10eb4837861fee4a3173b
Kazi Shahidul Islam <mindhillinformatics@gmail.com>
2018-06-14 14:33:48 +0400

    SUCCESS with oath passport - laravel (mySQL database) in cloud

--- AnywhereEverywhere/SigninSignupViewController.swift
+++ AnywhereEverywhere/SigninSignupViewController.swift
@@ -17,21 +17,31 @@ class SigninSignupViewController: UIViewController {
 
         btnEmailID.isHidden = true
         
-        let jsonUrlString = "https://www.appae.net/grabacab/public/api/provider/oauth/token"
-        guard let url = URL(string: jsonUrlString) else { return }
+        let urlString = "https://www.appae.net/grabacab/public/api/provider/oauth/token"
+        guard let requestUrl = URL(string:urlString) else { return }
+        let devid = UIDevice.current.identifierForVendor!.uuidString
+        let parameters = "email=testdriver@gmail.com&password=Test@123&device_token=1232324234234&device_id=\(devid)&device_type=ios"
+        var request = URLRequest(url: requestUrl)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-unlencoded;charset=utf-8", forHTTPHeaderField: "Cpntent-Type")
+        request.httpBody = parameters.data(using: String.Encoding.utf8)
         
-        URLSession.shared.dataTask(with: url) {(data, response, error) in
-            guard let data = data else { return }
-            let dataString = String(data: data, encoding: .utf8)
-            print(dataString)
+        //let jsonUrlString = "https://www.appae.net/grabacab/public/api/provider/oauth/token?email=testdriver@gmail.com&password=Test@123&device_token=1232324234234&device_id=\(devid)&device_type=ios"
+        //email":_emailStr,@"password":_passwordText.text, @"device_token":@"1232324234234",@"device_id":appDelegate.device_UDID , @"device_type":@"ios
         
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            if error == nil, let usableData = data {
+                var json: [String: Any]?
+                do {
+                    json = try JSONSerialization.jsonObject(with: usableData) as? [String:Any]
+                    print(json?["email"]! as Any)
+                    print(json?["id"]! as Any)
+                    print(data)
+                }catch{
+                    print(error)
+                }
+            }
         }.resume()
-        
-        
-        
-        
-        
-        
     }
 
     override func didReceiveMemoryWarning() {
